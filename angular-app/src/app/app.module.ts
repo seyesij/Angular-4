5a67c56869526c695083fea135812c2f9893d534
Dan Martin <dmartss13@gmail.com>
2017-09-09 22:36:54 -0400

    diary component rendering on main page

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DiaryComponent } from './diary/diary.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-app/src/app/diary/diary.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-diary',
+    templateUrl: './diary.component.html',
+})
+export class DiaryComponent {
+}
