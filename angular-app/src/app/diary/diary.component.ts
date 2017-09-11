3fa650141b1838250870247c3892dabd4ec144f4
Dan Martin <dmartss13@gmail.com>
2017-09-11 13:26:25 -0400

    insert input to the diary

--- angular-app/src/app/diary/diary.component.ts
+++ angular-app/src/app/diary/diary.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Input,
+    ViewChild,
+    ElementRef,
+    ContentChild
+} from '@angular/core';
 
 @Component({
     selector: 'app-diary',
     templateUrl: './diary.component.html',
+    styleUrls: ['./diary.component.css'],
 })
 export class DiaryComponent {
-    postNumber = 1;
-    // inputs = [];
+    // tslint:disable-next-line:no-input-rename (ts lint kept giving me a red underline so had to disable it here)
+    @Input('inputElement') element: {type: string, content: string};
+    @Input() name: string;
+    @ViewChild('heading') header: ElementRef;
+    @ContentChild('diaryContent') paragraph: ElementRef;
     constructor() {
 
     }
+}
     // handleUserInput() {
     //     if
     // }
-}
